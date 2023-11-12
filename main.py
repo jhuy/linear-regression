2da50e4f7691705ddc0d47af9f39607bfa15a488
jhuy <35741619+jhuy@users.noreply.github.com>
2023-11-12 14:11:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 # Initialize parameters of code
 lowest_int = 1
 highest_int = 5
-num_rand_ints = 100
+num_rand_ints = 300
 noise_factor = 1.5
-num_interations_of_regression = 50
+num_interations_of_regression = 100
 
 # intercept
-theta0 = 0
+theta0 = 2
 # slope
 theta1 = 0
 # learning rate
-alpha = 0.1
+alpha = 0.12
 
 # Generates random numbers to be linear regressed
 x_coor = np.linspace(lowest_int,highest_int, num = num_rand_ints)
@@ -40,8 +40,9 @@ plt.show()
 def new_theta_function (alpha, theta0, theta1, x, y):
     new_theta0 = float(theta0 - (alpha/num_rand_ints) * sum_theta_function_x(theta0, theta1, x, y))
     new_theta1 = float(theta1 - (alpha/num_rand_ints) * sum_theta_function_y(theta0, theta1, x, y))
-    print ([new_theta0, new_theta1])
-    return [new_theta0, new_theta1]
+    cost = (1/(2*num_rand_ints)) * sum_y_coordinates(theta0, theta1, x, y)
+    print ([new_theta0, new_theta1, cost])
+    return [new_theta0, new_theta1, cost]
 
 #sum(theta0 + theta1*x - y)
 def sum_theta_function_x (theta0, theta1, x, y):
@@ -57,5 +58,23 @@ def sum_theta_function_y (theta0, theta1, x, y):
         total += float((theta0 + theta1*x.iloc[index] - y.iloc[index])*x.iloc[index])
     return total
 
+def sum_y_coordinates (theta0, theta1, x, y):
+    total = 0
+    for index in range (0, num_rand_ints):
+        total += (theta0*x.iloc[index] + theta1 - y.iloc[index])**2
+    return total
+
+# Main
+# Lowest Cost Tracker
+[lowest_theta0, lowest_theta1, lowest_cost] = [0, 0, 0]
 for index in range(num_interations_of_regression):
-    [theta0, theta1] = new_theta_function(alpha, theta0, theta1, df.loc[:, 'x'], df.loc[:, 'y'])
+    [theta0, theta1, cost] = new_theta_function(alpha, theta0, theta1, df.loc[:, 'x'], df.loc[:, 'y'])
+    if index == 0:
+        lowest_cost = cost
+    if index > 0:
+        if lowest_cost > cost:
+            lowest_cost = cost
+            lowest_theta0 = theta0
+            lowest_theta1 = theta1
+
+print("The best theta parameters based on the lowest cost of: " + str(lowest_cost) + " is theta0: " + str(lowest_theta0) + " and theta1: " + str(lowest_theta1))
