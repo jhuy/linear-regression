f4f1b0138ae31471bead33999daf673fe10f3757
jhuy <35741619+jhuy@users.noreply.github.com>
2023-11-12 14:28:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ lowest_int = 1
 highest_int = 5
 num_rand_ints = 300
 noise_factor = 1.5
-num_interations_of_regression = 100
+num_interations_of_regression = 1000
 
 # intercept
 theta0 = 2
@@ -41,7 +41,7 @@ def new_theta_function (alpha, theta0, theta1, x, y):
     new_theta0 = float(theta0 - (alpha/num_rand_ints) * sum_theta_function_x(theta0, theta1, x, y))
     new_theta1 = float(theta1 - (alpha/num_rand_ints) * sum_theta_function_y(theta0, theta1, x, y))
     cost = (1/(2*num_rand_ints)) * sum_y_coordinates(theta0, theta1, x, y)
-    print ([new_theta0, new_theta1, cost])
+    # print ([new_theta0, new_theta1, cost])
     return [new_theta0, new_theta1, cost]
 
 #sum(theta0 + theta1*x - y)
@@ -67,6 +67,8 @@ def sum_y_coordinates (theta0, theta1, x, y):
 # Main
 # Lowest Cost Tracker
 [lowest_theta0, lowest_theta1, lowest_cost] = [0, 0, 0]
+
+print ("Computing theta0 and theta1 " + str(num_interations_of_regression) + " times, please wait.")
 for index in range(num_interations_of_regression):
     [theta0, theta1, cost] = new_theta_function(alpha, theta0, theta1, df.loc[:, 'x'], df.loc[:, 'y'])
     if index == 0:
